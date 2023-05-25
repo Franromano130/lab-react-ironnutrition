b269bc56c50bd9578ba2bb3a483d8befbb2652df
Francisco Romano <franromano130@gmail.com>
2023-05-25 10:28:24 +0200

    done lab

--- src/App.js
+++ src/App.js
@@ -1,65 +1,68 @@
 import { useState } from 'react';
 import { Row, Divider, Button } from 'antd';
-import data from "./foods.json";
+import data from './foods.json';
 import FoodBox from './components/FoodBox';
 import AddFoodForm from './components/AddFoodForm';
 import Search from './components/Search';
 import './App.css';
 
 function App() {
-
-  const [ foods, setFoods ] = useState(data)
-  const [ food, setFood ] = useState({
+  const [foods, setFoods] = useState(data);
+  const [food, setFood] = useState({
     name: '',
     calories: Number,
     image: '',
-    servings: Number
-  })
+    servings: Number,
+  });
 
-  const [ hide, setHide] = useState(false)
+  const [hide, setHide] = useState(false);
 
-  const [ searchInput, setSearchInput ] = useState('')
+  const [searchInput, setSearchInput] = useState('');
 
-  let filtered = searchInput ? [...foods].filter(food => food.name.toLowerCase().includes(searchInput.toLowerCase())) : foods
-
-
+  let filtered = searchInput
+    ? [...foods].filter((food) =>
+        food.name.toLowerCase().includes(searchInput.toLowerCase())
+      )
+    : foods;
 
   return (
     <div className="App">
+      <div className="top">
+        {!hide && (
+          <AddFoodForm
+            food={food}
+            setFood={setFood}
+            foods={foods}
+            setFoods={setFoods}
+          />
+        )}
 
-      <div className='top'>
-
-        {!hide && <AddFoodForm  food={food} setFood={setFood} foods={foods} setFoods={setFoods} /> }
-
-        <Button onClick={()=>setHide(!hide)}> Hide Form / Add New Food </Button>
+        <Button onClick={() => setHide(!hide)}>
+          {' '}
+          Hide Form / Add New Food{' '}
+        </Button>
 
         <Search setSearchInput={setSearchInput} />
-
       </div>
-
 
       <Divider>Food List</Divider>
 
+      <Row style={{ width: '100%', justifyContent: 'center' }}>
+        {filtered.map((food) => {
+          return (
+            <FoodBox
+              key={food.name}
+              food={food}
+              foods={foods}
+              setFoods={setFoods}
+            />
+          );
+        })}
+      </Row>
 
-        <Row style={{ width: '100%', justifyContent: 'center' }}>
-
-            
-            {
-              filtered.map((food) => {
-                return <FoodBox key={food.name} food={food} foods={foods} setFoods={setFoods} />
-              })
-            }
-
-        </Row>
-
-
-        {!filtered.length && <h3>There is no more food to show.</h3>}
-
-
-
+      {!filtered.length && <h3>No hay mas comida para mostrar.</h3>}
     </div>
   );
 }
 
 export default App;
-
